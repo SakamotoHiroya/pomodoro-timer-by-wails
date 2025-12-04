8c0bec6c46cc67c0998051725662d2f85268690c
SakamotoHiroya <88700870+SakamotoHiroya@users.noreply.github.com>
2025-12-04 19:34:02 +0900

    Remove unused import from index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import styles from './index.module.css'
-import { Greet } from '../wailsjs/wailsjs/go/main/App'
 
 const Home: NextPage = () => {
 
